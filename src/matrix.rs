4082df769957fc70d85f0251eab6465bc2f7e44d
Tom Moscrip <26145717+tmoscrip@users.noreply.github.com>
2025-06-07 20:59:07 +0100

    feat: matrix inversion

--- src/matrix.rs
+++ src/matrix.rs
@@ -49,8 +49,11 @@ impl Matrix {
         if self.rows == 2 && self.cols == 2 {
             self.data[0][0] * self.data[1][1] - self.data[0][1] * self.data[1][0]
         } else {
-            // TODO: Implement determinant for larger matrices
-            0.0
+            let mut determinant = 0.0;
+            for col in 0..self.cols {
+                determinant += self.data[0][col] * self.cofactor(0, col);
+            }
+            determinant
         }
     }
 
@@ -75,6 +78,45 @@ impl Matrix {
                 result_col += 1;
             }
             result_row += 1;
+        }
+
+        result
+    }
+
+    pub fn minor(&self, row: usize, col: usize) -> f64 {
+        let sub = self.submatrix(row, col);
+        return sub.determinant();
+    }
+
+    pub fn cofactor(&self, row: usize, col: usize) -> f64 {
+        let minor = self.minor(row, col);
+        if (row + col) % 2 == 0 {
+            minor
+        } else {
+            -minor
+        }
+    }
+
+    pub fn inverse(&self) -> Matrix {
+        let det = self.determinant();
+        if det == 0.0 {
+            panic!("Matrix is not invertible");
+        }
+
+        let mut cofactor_matrix = Matrix::new(self.rows, self.cols);
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                cofactor_matrix[(row, col)] = self.cofactor(row, col);
+            }
+        }
+
+        let transposed_cofactors = cofactor_matrix.transpose();
+
+        let mut result = Matrix::new(self.rows, self.cols);
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                result[(row, col)] = transposed_cofactors[(row, col)] / det;
+            }
         }
 
         result
@@ -156,6 +198,31 @@ impl Mul<Tuple> for Matrix {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use approx::{assert_abs_diff_eq, AbsDiffEq};
+
+    impl AbsDiffEq for Matrix {
+        type Epsilon = f64;
+
+        fn default_epsilon() -> Self::Epsilon {
+            f64::EPSILON
+        }
+
+        fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+            if self.rows != other.rows || self.cols != other.cols {
+                return false;
+            }
+
+            for row in 0..self.rows {
+                for col in 0..self.cols {
+                    if !f64::abs_diff_eq(&self.data[row][col], &other.data[row][col], epsilon) {
+                        return false;
+                    }
+                }
+            }
+
+            true
+        }
+    }
 
     #[test]
     fn constructing_and_inspecting_4x4_matrix() {
@@ -362,5 +429,177 @@ mod tests {
         ]);
 
         assert_eq!(matrix_a.submatrix(2, 1), expected);
+    }
+
+    #[test]
+    fn calculating_minor_of_3x3_matrix() {
+        let matrix_a = Matrix::from_vec(vec![
+            vec![3.0, 5.0, 0.0],
+            vec![2.0, -1.0, -7.0],
+            vec![6.0, -1.0, 5.0],
+        ]);
+
+        let submatrix_b = matrix_a.submatrix(1, 0);
+        assert_eq!(submatrix_b.determinant(), 25.0);
+        assert_eq!(matrix_a.minor(1, 0), 25.0);
+    }
+
+    #[test]
+    fn calculating_cofactor_of_3x3_matrix() {
+        let matrix_a = Matrix::from_vec(vec![
+            vec![3.0, 5.0, 0.0],
+            vec![2.0, -1.0, -7.0],
+            vec![6.0, -1.0, 5.0],
+        ]);
+
+        assert_eq!(matrix_a.minor(0, 0), -12.0);
+        assert_eq!(matrix_a.cofactor(0, 0), -12.0);
+        assert_eq!(matrix_a.minor(1, 0), 25.0);
+        assert_eq!(matrix_a.cofactor(1, 0), -25.0);
+    }
+
+    #[test]
+    fn calculating_determinant_of_3x3_matrix() {
+        let matrix_a = Matrix::from_vec(vec![
+            vec![1.0, 2.0, 6.0],
+            vec![-5.0, 8.0, -4.0],
+            vec![2.0, 6.0, 4.0],
+        ]);
+
+        assert_eq!(matrix_a.cofactor(0, 0), 56.0);
+        assert_eq!(matrix_a.cofactor(0, 1), 12.0);
+        assert_eq!(matrix_a.cofactor(0, 2), -46.0);
+        assert_eq!(matrix_a.determinant(), -196.0);
+    }
+
+    #[test]
+    fn calculating_determinant_of_4x4_matrix() {
+        let matrix_a = Matrix::from_vec(vec![
+            vec![-2.0, -8.0, 3.0, 5.0],
+            vec![-3.0, 1.0, 7.0, 3.0],
+            vec![1.0, 2.0, -9.0, 6.0],
+            vec![-6.0, 7.0, 7.0, -9.0],
+        ]);
+
+        assert_eq!(matrix_a.cofactor(0, 0), 690.0);
+        assert_eq!(matrix_a.cofactor(0, 1), 447.0);
+        assert_eq!(matrix_a.cofactor(0, 2), 210.0);
+        assert_eq!(matrix_a.cofactor(0, 3), 51.0);
+        assert_eq!(matrix_a.determinant(), -4071.0);
+    }
+
+    #[test]
+    fn testing_invertible_matrix_for_invertibility() {
+        let matrix_a = Matrix::from_vec(vec![
+            vec![6.0, 4.0, 4.0, 4.0],
+            vec![5.0, 5.0, 7.0, 6.0],
+            vec![4.0, -9.0, 3.0, -7.0],
+            vec![9.0, 1.0, 7.0, -6.0],
+        ]);
+
+        assert_eq!(matrix_a.determinant(), -2120.0);
+    }
+
+    #[test]
+    fn testing_noninvertible_matrix_for_invertibility() {
+        let matrix_a = Matrix::from_vec(vec![
+            vec![-4.0, 2.0, -2.0, -3.0],
+            vec![9.0, 6.0, 2.0, 6.0],
+            vec![0.0, -5.0, 1.0, -5.0],
+            vec![0.0, 0.0, 0.0, 0.0],
+        ]);
+
+        assert_eq!(matrix_a.determinant(), 0.0);
+    }
+
+    #[test]
+    fn calculating_inverse_of_matrix() {
+        let matrix_a = Matrix::from_vec(vec![
+            vec![-5.0, 2.0, 6.0, -8.0],
+            vec![1.0, -5.0, 1.0, 8.0],
+            vec![7.0, 7.0, -6.0, -7.0],
+            vec![1.0, -3.0, 7.0, 4.0],
+        ]);
+
+        let b = matrix_a.inverse();
+
+        assert_eq!(matrix_a.determinant(), 532.0);
+        assert_eq!(matrix_a.cofactor(2, 3), -160.0);
+        assert!((b[(3, 2)] - (-160.0 / 532.0)).abs() < f64::EPSILON);
+        assert_eq!(matrix_a.cofactor(3, 2), 105.0);
+        assert!((b[(2, 3)] - (105.0 / 532.0)).abs() < f64::EPSILON);
+
+        let expected = Matrix::from_vec(vec![
+            vec![0.21805, 0.45113, 0.24060, -0.04511],
+            vec![-0.80827, -1.45677, -0.44361, 0.52068],
+            vec![-0.07895, -0.22368, -0.05263, 0.19737],
+            vec![-0.52256, -0.81391, -0.30075, 0.30639],
+        ]);
+
+        assert_abs_diff_eq!(b, expected, epsilon = 0.0001);
+    }
+
+    #[test]
+    fn calculating_inverse_of_another_matrix() {
+        let matrix_a = Matrix::from_vec(vec![
+            vec![8.0, -5.0, 9.0, 2.0],
+            vec![7.0, 5.0, 6.0, 1.0],
+            vec![-6.0, 0.0, 9.0, 6.0],
+            vec![-3.0, 0.0, -9.0, -4.0],
+        ]);
+
+        let b = matrix_a.inverse();
+
+        let expected = Matrix::from_vec(vec![
+            vec![-0.15385, -0.15385, -0.28205, -0.53846],
+            vec![-0.07692, 0.12308, 0.02564, 0.03077],
+            vec![0.35897, 0.35897, 0.43590, 0.92308],
+            vec![-0.69231, -0.69231, -0.76923, -1.92308],
+        ]);
+
+        assert_abs_diff_eq!(b, expected, epsilon = 0.0001);
+    }
+
+    #[test]
+    fn calculating_inverse_of_third_matrix() {
+        let matrix_a = Matrix::from_vec(vec![
+            vec![9.0, 3.0, 0.0, 9.0],
+            vec![-5.0, -2.0, -6.0, -3.0],
+            vec![-4.0, 9.0, 6.0, 4.0],
+            vec![-7.0, 6.0, 6.0, 2.0],
+        ]);
+
+        let b = matrix_a.inverse();
+
+        let expected = Matrix::from_vec(vec![
+            vec![-0.04074, -0.07778, 0.14444, -0.22222],
+            vec![-0.07778, 0.03333, 0.36667, -0.33333],
+            vec![-0.02901, -0.14630, -0.10926, 0.12963],
+            vec![0.17778, 0.06667, -0.26667, 0.33333],
+        ]);
+
+        assert_abs_diff_eq!(b, expected, epsilon = 0.0001);
+    }
+
+    #[test]
+    fn multiplying_product_by_its_inverse() {
+        let matrix_a = Matrix::from_vec(vec![
+            vec![3.0, -9.0, 7.0, 3.0],
+            vec![3.0, -8.0, 2.0, -9.0],
+            vec![-4.0, 4.0, 4.0, 1.0],
+            vec![-6.0, 5.0, -1.0, 1.0],
+        ]);
+
+        let matrix_b = Matrix::from_vec(vec![
+            vec![8.0, 2.0, 2.0, 2.0],
+            vec![3.0, -1.0, 7.0, 0.0],
+            vec![7.0, 0.0, 5.0, 4.0],
+            vec![6.0, -2.0, 0.0, 5.0],
+        ]);
+
+        let c = matrix_a.clone() * matrix_b.clone();
+        let result = c * matrix_b.inverse();
+
+        assert_abs_diff_eq!(result, matrix_a, epsilon = 0.0001);
     }
 }
