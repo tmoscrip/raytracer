388a7b4ed1713cc4c6f202b1133bdc069f6935fa
Tom Moscrip <26145717+tmoscrip@users.noreply.github.com>
2025-06-07 18:26:53 +0100

    feat: matrix multiplication

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,6 +1,8 @@
-use std::ops::{Index, IndexMut};
+use std::ops::{Index, IndexMut, Mul};
 
-#[derive(Debug)]
+use crate::tuple::Tuple;
+
+#[derive(Debug, Clone)]
 pub struct Matrix {
     data: Vec<Vec<f64>>,
     rows: usize,
@@ -20,6 +22,15 @@ impl Matrix {
         let rows = data.len();
         let cols = if rows > 0 { data[0].len() } else { 0 };
         Matrix { data, rows, cols }
+    }
+
+    pub fn identity() -> Self {
+        let mut matrix = Matrix::new(4, 4);
+        matrix.data[0][0] = 1.0;
+        matrix.data[1][1] = 1.0;
+        matrix.data[2][2] = 1.0;
+        matrix.data[3][3] = 1.0;
+        matrix
     }
 }
 
@@ -53,6 +64,45 @@ impl PartialEq for Matrix {
         }
 
         true
+    }
+}
+
+impl Mul<Matrix> for Matrix {
+    type Output = Self;
+
+    fn mul(self, rhs: Matrix) -> Matrix {
+        let mut result = Matrix::new(self.rows, rhs.cols);
+
+        for row in 0..self.rows {
+            for col in 0..rhs.cols {
+                let mut sum = 0.0;
+                for k in 0..self.cols {
+                    sum += self.data[row][k] * rhs.data[k][col];
+                }
+                result.data[row][col] = sum;
+            }
+        }
+
+        result
+    }
+}
+
+impl Mul<Tuple> for Matrix {
+    type Output = Tuple;
+
+    fn mul(self, rhs: Tuple) -> Self::Output {
+        let tuple_vec = vec![rhs.x, rhs.y, rhs.z, rhs.w];
+        let mut result = vec![0.0; self.rows];
+
+        for row in 0..self.rows {
+            let mut sum = 0.0;
+            for col in 0..self.cols {
+                sum += self.data[row][col] * tuple_vec[col];
+            }
+            result[row] = sum;
+        }
+
+        Tuple::new(result[0], result[1], result[2], result[3])
     }
 }
 
@@ -137,5 +187,68 @@ mod tests {
         ]);
 
         assert_ne!(matrix_a, matrix_b);
+    }
+
+    #[test]
+    fn matrix_can_be_multiplied_by_matrix() {
+        let matrix_a = Matrix::from_vec(vec![
+            vec![1.0, 2.0, 3.0, 4.0],
+            vec![5.0, 6.0, 7.0, 8.0],
+            vec![9.0, 8.0, 7.0, 6.0],
+            vec![5.0, 4.0, 3.0, 2.0],
+        ]);
+
+        let matrix_b = Matrix::from_vec(vec![
+            vec![-2.0, 1.0, 2.0, 3.0],
+            vec![3.0, 2.0, 1.0, -1.0],
+            vec![4.0, 3.0, 6.0, 5.0],
+            vec![1.0, 2.0, 7.0, 8.0],
+        ]);
+
+        let expected = Matrix::from_vec(vec![
+            vec![20.0, 22.0, 50.0, 48.0],
+            vec![44.0, 54.0, 114.0, 108.0],
+            vec![40.0, 58.0, 110.0, 102.0],
+            vec![16.0, 26.0, 46.0, 42.0],
+        ]);
+
+        assert_eq!(matrix_a * matrix_b, expected);
+    }
+
+    #[test]
+    fn matrix_can_be_multiplied_by_tuple() {
+        let matrix_a = Matrix::from_vec(vec![
+            vec![1.0, 2.0, 3.0, 4.0],
+            vec![2.0, 4.0, 4.0, 2.0],
+            vec![8.0, 6.0, 4.0, 1.0],
+            vec![0.0, 0.0, 0.0, 1.0],
+        ]);
+
+        let tuple_b = Tuple::new(1.0, 2.0, 3.0, 1.0);
+        let expected = Tuple::new(18.0, 24.0, 33.0, 1.0);
+
+        assert_eq!(matrix_a * tuple_b, expected);
+    }
+
+    #[test]
+    fn matrix_multiplied_by_identity_matrix_equals_itself() {
+        let matrix_a = Matrix::from_vec(vec![
+            vec![0.0, 1.0, 2.0, 4.0],
+            vec![1.0, 2.0, 4.0, 8.0],
+            vec![2.0, 4.0, 8.0, 16.0],
+            vec![4.0, 8.0, 16.0, 32.0],
+        ]);
+
+        let identity = Matrix::identity();
+
+        assert_eq!(matrix_a.clone() * identity, matrix_a);
+    }
+
+    #[test]
+    fn identity_matrix_multiplied_by_tuple_equals_tuple() {
+        let tuple_a = Tuple::new(1.0, 2.0, 3.0, 4.0);
+        let identity = Matrix::identity();
+
+        assert_eq!(identity * tuple_a, tuple_a);
     }
 }
